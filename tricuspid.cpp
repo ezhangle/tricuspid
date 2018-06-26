306b000beaaa415d1d9ebe3c02393b76c4a9767d
Pierre Abbat <phma@bezitopo.org>
2018-06-26 07:38:09 +0000

    Initial commit
    The program is a stub that just outputs "tricuspid".

---
+++ tricuspid.cpp
@@ -0,0 +1,14 @@
+/******************************************************/
+/*                                                    */
+/* tricuspid.cpp - a curve with three cusps per side  */
+/*                                                    */
+/******************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  cout<<"tricuspid\n";
+  return 0;
+}
